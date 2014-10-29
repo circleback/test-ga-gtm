8b3985e3125e724e76f43d139a4395e51c9bfbc5
Fabricio Quagliariello <matias.quaglia@gmail.com>
2014-10-29 10:53:14 -0300

    initial load

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+  .module('testGaGtmApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true).hashPrefix('!');
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
