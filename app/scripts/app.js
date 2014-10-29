30ddf6cb7958f5a92350ba1d6b8a9ce80b2303ad
Fabricio Quagliariello <matias.quaglia@gmail.com>
2014-10-29 11:52:42 -0300

    added google analytics and google tag manager modules

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,12 +5,24 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'angulartics',
+    'angulartics.google.analytics',
+    'angulartics.google.tagmanager'
   ])
-  .config(function ($routeProvider, $locationProvider) {
+  .config(function ($routeProvider, $locationProvider, $analyticsProvider) {
     $locationProvider.html5Mode(true).hashPrefix('!');
+    $analyticsProvider.virtualPageviews(true);
     $routeProvider
       .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/contact', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('testGaGtmApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $analytics) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.test = function() {
+      $analytics.eventTrack(
+        'test method',
+        {
+          'category': 'button',
+          'label': 'test'
+        }
+      )
+    };
   });
